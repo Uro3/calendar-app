0d89f76c68a4e10e2f5b6037f769ba79a3510cba
Uro3 <miyakawa.uro3@gmail.com>
2021-05-21 23:06:13 +0900

    add other dialog components

--- src/components/ScheduleDialog/ScheduleDialog.tsx
+++ src/components/ScheduleDialog/ScheduleDialog.tsx
@@ -3,6 +3,8 @@ import { useAppSelector, useAppDispatch } from '../../hooks';
 import { ScheduleDialogState } from '../../types';
 import { close } from '../../modules/scheduleDialog';
 import ScheduleDialogAdd from './ScheduleDialogAdd';
+import ScheduleDialogUpdate from './ScheduleDialogUpdate';
+import ScheduleDialogRemove from './ScheduleDialogRemove';
 
 const ScheduleDialog: React.FC = () => {
   const scheduleDialog: ScheduleDialogState = useAppSelector<ScheduleDialogState>(state => state.scheduleDialog);
@@ -26,9 +28,23 @@ const ScheduleDialog: React.FC = () => {
       break;
 
     case 'UPDATE':
+      isActiveClass = 'is-active';
+      content = <ScheduleDialogUpdate
+        close={closeDialog}
+        year={scheduleDialog.year}
+        month={scheduleDialog.month}
+        date={scheduleDialog.date}
+      />;
       break;
 
     case 'REMOVE':
+      isActiveClass = 'is-active';
+      content = <ScheduleDialogRemove
+        close={closeDialog}
+        year={scheduleDialog.year}
+        month={scheduleDialog.month}
+        date={scheduleDialog.date}
+      />;
       break;
 
     default:
