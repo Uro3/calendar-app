95cae9014b6ed88b7cb7c192ab4a0e78c47dd5d2
Uro3 <miyakawa.uro3@gmail.com>
2021-05-22 00:16:53 +0900

    enable redux devtools

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { calendarReducer } from './modules/calendar';
 import { scheduleReducer } from './modules/schedule';
 import { scheduleDialogReducer } from './modules/scheduleDialog';
+
+// https://qiita.com/AshSuzuki/items/111d5a7c5d30fd1123c3
+interface ExtendedWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+}
+declare var window: ExtendedWindow;
+const composeReduxDevToolsEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
   calendar: calendarReducer,
@@ -9,7 +16,10 @@ const rootReducer = combineReducers({
   scheduleDialog: scheduleDialogReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeReduxDevToolsEnhancers()
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
